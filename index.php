a516dd4ee29a8a2d39b0d15ce1030f6ad39880a3
Enrico Deiana <enry537@gmail.com>
2015-01-19 12:33:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-	if ( (@$_GET['login'] == "success") ||  (@$_GET['insert'] == "success") || (@$_GET['logout'] == "success")) {
+	if ( (if(isset($_GET["login"])) $_GET['login'] == "success") ||  (if(isset($_GET["insert"])) $_GET['insert'] == "success") || (if(isset($_GET["logout"])) $_GET['logout'] == "success")) {
 	header( "refresh:1;url={$_SERVER['PHP_SELF']}" ); 
 }
 ?>
